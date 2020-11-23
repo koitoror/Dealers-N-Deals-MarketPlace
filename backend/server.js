9953ed1cb1625272064f4338923fd8ab176ff587
Daniel Kamar <kamarster@gmail.com>
2020-11-23 21:59:47 +0300

    add runnig backend api  server

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import morgan from 'morgan'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
+
+dotenv.config()
+
+const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(express.json())
+
+
+if (process.env.NODE_ENV === 'development') {
+  
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
