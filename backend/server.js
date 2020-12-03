7c37e7e2ad603368575bd1ac1e6f426b052978b4
Daniel Kamar <kamarster@gmail.com>
2020-12-03 18:20:44 +0300

    add dependies and expose API product routes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import userRoutes from './routes/userRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -19,13 +21,22 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json())
 
 app.use('/api/users', userRoutes)
+app.use('/api/products', productRoutes)
+app.use('/api/upload', uploadRoutes)
 
-if (process.env.NODE_ENV === 'development') {
-  
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  // app.get('*', (req, res) =>
+  //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  // )
+} else {
   app.get('/', (req, res) => {
     res.send('API is running....')
   })
 }
+
 
 app.use(notFound)
 app.use(errorHandler)
